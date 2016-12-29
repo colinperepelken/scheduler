2b04691cb34a04bb43fa153fc3a6dc2fb81c5617
Colin <Colin>
2016-12-28 17:16:39 -0800

    added ability to update employees. created css file and applied to employee.php

--- employee.php
+++ employee.php
@@ -8,11 +8,34 @@ $pdo = (new SQLiteConnection())->connect();
 
 <head>
 <title>KVLiquor - Employee</title>
+<link rel='stylesheet' type='text/css' href='style/kvliquor.css'/>
 </head>
 <body>
+<div id = "header">
+<img src="http://i.imgur.com/rCYjjsD.jpg" style="float:left">
+
+<div id="links">
+
+<table id="top"><tr>
+<td><a href = "admin.php"> Back To Home </a></td></tr></table>
+</div>
+</div>
 
 <?php
+echo "<a href=\"admin.php\"> Back to Admin";
 
+/* CHECKS IF FIRST NAME AND LAST NAME ARE SET */
+function validate() {
+	if(isset($_GET['firstname']) && isset($_GET['lastname'])) {
+		if(!empty($_GET['firstname']) && !empty($_GET['lastname'])) {
+			return true;
+		}
+	}
+	return false;
+}
+
+
+$id = null;
 if(isset($_GET['id'])) {
 	/* VIEWING AND EDITING A CURRENT EMPLOYEE */
 
@@ -30,32 +53,58 @@ if(isset($_GET['id'])) {
 	$username = $employee->username;
 	$phone = $employee->phone;
 	
-	$cmd = "update";
-
-} else if (isset($_GET['cmd'])) {
-	/* SAVE BUTTON HAS BEEN PRESSED */
-	$cmd = $_GET['cmd'];
-	if(!empty($_GET['firstname']) && !empty($_GET['lastname'])) {
-		if ($cmd == "update") {
-			echo "updating existing emp";
-		} else if ($cmd == "add") {
-			echo "adding new emp";
+	// check if save button has been pressed
+	if(isset($_GET['submitted'])) {
+		// update employee if firstname and lastname are not null
+		if(validate()) {
+			// update
+			$firstname = $_GET['firstname'];
+			$lastname = $_GET['lastname'];
+			$email = $_GET['email'];
+			$username = $_GET['username'];
+			$phone = $_GET['phone'];
+			
+			// TODO: ^^ strip symbols to prevent injections ^^
+			// TODO: validate phone format or change database design
+			
+			$sql = "UPDATE Employee 
+					SET firstname = :firstname, lastname = :lastname, 
+						email = :email, username = :username, phone = :phone
+					WHERE id = :id;";
+			$stmt = $pdo->prepare($sql);
+			
+			// passing values to the parameters
+			$stmt->bindValue(':firstname', $firstname);
+			$stmt->bindValue(':lastname', $lastname);
+			$stmt->bindValue(':email', $email);
+			$stmt->bindValue(':username', $username);
+			$stmt->bindValue(':phone', $phone);
+			$stmt->bindValue(':id', $id);
+			
+			$stmt->execute(); // execute the statement
+			$message = "Update Successful!";
+			echo "<script type='text/javascript'>alert('$message');</script>";
+		} else { // alert user
+			$message = "Invalid! Firstname and lastname must not be empty.";
+			echo "<script type='text/javascript'>alert('$message');</script>";
 		}
-	} else { // invalid! database requires first name and last name
-		$message = "Invalid! Firstname and Lastname must not be empty.";
-		echo "<script type='text/javascript'>alert('$message');</script>";
-		
 	}
-	
-	
 	
 } else {
 	/* ADDING A NEW EMPLOYEE */
 	echo "<h2>Add an Employee</h2>";
 	$firstname = $lastname = $email = $username = $phone = "";
-	$id = -1;
 	
-	$cmd = "add";
+	// check if save button has been pressed
+	if(isset($_GET['submitted'])) {
+		// insert employee if firstname and lastname are not null
+		if(validate()) {
+			echo 'inserting';
+		} else {
+			$message = "Invalid! Firstname and lastname must not be empty.";
+			echo "<script type='text/javascript'>alert('$message');</script>";
+		}
+	}
 }
 
 echo "<form method=\"get\" action=\"employee.php\">
@@ -63,10 +112,15 @@ echo "<form method=\"get\" action=\"employee.php\">
 		<tr><td align =\"left\">First Name:</td><td align =\"left\"><input type=\"text\" name=\"firstname\" size=\"30\" value=\"$firstname\"></td></td>
 		<tr><td  align =\"left\">Last Name:</td><td  align =\"left\"><input type=\"text\" name=\"lastname\" size=\"30\" value=\"$lastname\"></td></td>
 		<tr><td align =\"left\">Email:</td><td align =\"left\"><input type=\"text\" name=\"email\" size=\"30\" value=\"$email\"></td></td>
-		<tr><td align =\"left\">Site Username:</td><td align =\"left\"><input type=\"text\" name=\"wid\" size=\"30\" value=\"$username\"></td></td>
+		<tr><td align =\"left\">Site Username:</td><td align =\"left\"><input type=\"text\" name=\"username\" size=\"30\" value=\"$username\"></td></td>
 		<tr><td align =\"left\">Phone:</td><td align =\"left\"><input type=\"text\" name=\"phone\" size=\"10\" value=\"$phone\"></td></td>
-		</table>
-		<input type=\"hidden\" name=\"cmd\" value=\"$cmd\">
+		</table>";
+		
+if(!empty($id)) {
+	echo "<input type=\"hidden\" name=\"id\" value=\"$id\">";
+}
+		
+echo "<input type=\"hidden\" name=\"submitted\" value=\"1\">
 		<br><br>
 		<input type=\"submit\" name=\"submit\" value=\"Save\" id=\"submit\" />
 		</form>";
