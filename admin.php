204a34e422278f02dc951691b8941205ae908c72
Colin Bernard <colinjbernard@hotmail.com>
2017-01-09 22:05:58 -0800

    can now change months of calendar

--- admin.php
+++ admin.php
@@ -52,27 +52,49 @@ function alert($message) {
 <td><a href = "settings.php"> Settings </a></td></tr></table>
 </div>
 </div>
-
-
+<?php
+if(isset($_GET['day'])) {
+	$day = $_GET['day'];
+} else {
+	$day = 0;
+}
+?>
 <div id = "calendar">
 <script language="javascript" type="text/javascript">
+var cal = ""; // used for printing
+
+
 var day_of_week = new Array('Sun','Mon','Tue','Wed','Thu','Fri','Sat');
 var month_of_year = new Array('January','February','March','April','May','June','July','August','September','October','November','December');
 
 //  DECLARE AND INITIALIZE VARIABLES
 var Calendar = new Date();
 
-var year = Calendar.getFullYear();     // Returns year
-var month = Calendar.getMonth();    // Returns month (0-11)
+var year = <?php echo $_GET['year']; ?>;     // Returns year
+var month = <?php echo $_GET['month'] - 1; ?>;    // Returns month (0-11)
 var today = Calendar.getDate();    // Returns day (1-31)
-var weekday = Calendar.getDay();    // Returns day (1-31)
+var weekday = <?php echo $_GET['day']; ?>    // Returns day (1-31)
+
+// next and previous month buttons
+if(month == 0) {
+	cal += "<button><a href=\"admin.php?year=" + (year-1) +"&month=" + 12 + "&day=1\">Last Month</a></button>"; // previous month
+	cal += "<button style=\"float: right;\"><a href=\"admin.php?year=" + year +"&month=" + (month+2) + "&day=1\">Next Month</a></button>"; // next month
+} else if(month == 11) {
+	cal = "<button><a href=\"admin.php?year=" + year +"&month=" + (month) + "&day=1\">Last Month</a></button>"; // previous month
+	cal += "<button style=\"float: right;\"><a href=\"admin.php?year=" + (year+1) +"&month=" + 1 + "&day=1\">Next Month</a></button>"; // next month
+} else {
+	cal = "<button><a href=\"admin.php?year=" + year +"&month=" + (month) + "&day=1\">Last Month</a></button>"; // previous month
+	cal += "<button style=\"float: right;\"><a href=\"admin.php?year=" + year +"&month=" + (month+2) + "&day=1\">Next Month</a></button>"; // next month
+}
 
 var DAYS_OF_WEEK = 7;    // "constant" for number of days in a week
 var DAYS_OF_MONTH = 31;    // "constant" for number of days in a month
-var cal;    // Used for printing
 
 Calendar.setDate(1);    // Start the calendar day at '1'
 Calendar.setMonth(month);    // Start the calendar month at now
+
+
+
 
 
 /* VARIABLES FOR FORMATTING
@@ -85,12 +107,14 @@ var highlight_start = '<TD WIDTH="60" HEIGHT="50"><TABLE CELLSPACING=0 BORDER=1 
 var highlight_end   = '</CENTER></TD></TR></TABLE></B>';
 var TD_start = '<TD WIDTH="60" HEIGHT="50"><CENTER>';
 var TD_end = '</CENTER></TD>';
+var selected_start = "<div id=\"selected\">";
+var selected_end = "</div>";
 
 /* BEGIN CODE FOR CALENDAR
 NOTE: You can format the 'BORDER', 'BGCOLOR', 'CELLPADDING', 'BORDERCOLOR'
 tags to customize your calendar's look.*/
 
-cal =  '<TABLE BORDER=1 CELLSPACING=0 CELLPADDING=0 BORDERCOLOR=BBBBBB><TR><TD>';
+cal +=  '<TABLE BORDER=1 CELLSPACING=0 CELLPADDING=0 BORDERCOLOR=BBBBBB><TR><TD>';
 cal += '<TABLE BORDER=0 CELLSPACING=0 CELLPADDING=2>' + TR_start;
 cal += '<TD COLSPAN="' + DAYS_OF_WEEK + '" BGCOLOR="#EFEFEF"><CENTER><B>';
 cal += month_of_year[month]  + '   ' + year + '</B>' + TD_end + TR_end;
@@ -103,12 +127,13 @@ for(index=0; index < DAYS_OF_WEEK; index++)
 {
 
 // BOLD TODAY'S DAY OF WEEK
-if(weekday == index)
+if(weekday == index) {
 cal += TD_start + '<B>' + day_of_week[index] + '</B>' + TD_end;
 
 // PRINTS DAY
-else
+}else{
 cal += TD_start + day_of_week[index] + TD_end;
+}
 }
 
 cal += TD_end + TR_end;
@@ -136,12 +161,21 @@ if( Calendar.getDate() > index )
   // SET VARIABLE INSIDE LOOP FOR INCREMENTING PURPOSES
   var day  = Calendar.getDate();
 
+  if(day == <?php echo $day; ?>) {
+	  if( today==Calendar.getDate() ) {
+		  cal += highlight_start + '<a href=admin.php?year=' + year + '&month=' + (month+1) + '&day=' + day + '>' + selected_start + day + selected_end + '</a>' + highlight_end + TD_end;
+	  } else {
+		   cal += TD_start + '<a href=admin.php?year=' + year + '&month=' + (month+1) + '&day=' + day + '>' + selected_start + day + selected_end + '</a>' + TD_end;
+		  
+	  }
+	 
+	  
   // HIGHLIGHT TODAY'S DATE
-  if( today==Calendar.getDate() )
+  } else if( today==Calendar.getDate() ) {
   cal += highlight_start + '<a href=admin.php?year=' + year + '&month=' + (month+1) + '&day=' + day + '>' + day + '</a>' + highlight_end + TD_end;
-
+	
   // PRINTS DAY
-  else
+  }else{
   cal += TD_start + '<a href=admin.php?year=' + year + '&month=' + (month+1) + '&day=' + day + '>' + day + '</a>' + TD_end;
   }
 
@@ -152,7 +186,7 @@ if( Calendar.getDate() > index )
 
   // INCREMENTS UNTIL END OF THE MONTH
   Calendar.setDate(Calendar.getDate()+1);
-
+}
 }// end for loop
 
 cal += '</TD></TR></TABLE></TABLE>';
